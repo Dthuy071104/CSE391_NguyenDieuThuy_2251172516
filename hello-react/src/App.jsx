0a84885c2d1f12693867117e6a6c24dc948768a8
Dthuy071104 <ndt071104@gmail.com>
2026-05-30 17:35:42 +0700

    Complete Tier 0.1 first component

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Nguyễn Diệu Thúy</h1>
+      <p>Hôm nay là ngày đẹp trời</p>
+
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
